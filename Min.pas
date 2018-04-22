b16a6e7739a9041d31dfcf71a522e89a40319dc8
axelbraunschweiger <axelbraunschweiger@gmail.com>
2018-04-22 17:39:07 -0500

    wrote program for determining lowest number

--- Min.pas
+++ Min.pas
@@ -1,1 +1,38 @@
 
+
+
+program Minimum (input, output);
+
+{reads integer input and prints the lowest number}
+
+
+var 
+number : integer;
+min : integer;
+
+begin
+
+writeln('enter full numbers to get the lowest number.');
+writeln('Exit with 0: ');
+readln(number);
+min := number;
+
+
+while number <> 0 do
+
+begin
+if number < min then
+min := number;
+readln(number)
+end ;
+
+if min <> 0 then
+writeln('lowest number is ', min)
+
+else 
+writeln('only a 0 was entered')
+
+
+
+
+end.
