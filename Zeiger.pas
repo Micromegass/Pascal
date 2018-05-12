b55cb33ba3dae7d68b3d155c42541fd952f4e6f2
axelbraunschweiger <axelbraunschweiger@gmail.com>
2018-05-12 15:30:54 -0500

    exercises on pointers

---
+++ Zeiger.pas
@@ -0,0 +1,75 @@
+
+program Zeiger (input, output);
+var
+Eingabe : integer;  
+
+
+
+
+
+type
+tRefListe = ^tListe;
+tListe = record
+		info: integer; 
+		next : tRefListe
+		end; 
+
+
+
+
+
+procedure ListeAufbauen (var outRefAnfang : tRefListe); 
+
+var 
+Zeiger: tRefliste;
+Zahl : integer; 
+
+
+begin 
+
+outRefAnfang := nil; 
+readln(Zahl);
+
+while Zahl <> 0 do 
+
+begin 
+new (Zeiger);
+Zeiger^.info := Zahl; 
+Zeiger^.next := outRefAnfang;
+outRefAnfang := Zeiger; 
+readln(Zahl)
+end 
+
+end; 
+
+procedure ListeDurchlaufen (inRefAnfang : tRefListe);
+
+var 
+Zeiger : tRefListe;
+
+
+begin 
+Zeiger := inRefAnfang;
+while Zeiger <> nil do 
+begin 
+writeln(Zeiger^.info); 
+Zeiger := Zeiger^.next 
+end 
+
+end; 
+ 
+ 
+ 
+begin 
+
+
+
+
+writeln('enter numbers: ');
+ListeAufbauen(Eingabe);
+
+
+ListeDurchlaufen(Eingabe);
+
+end.
+
