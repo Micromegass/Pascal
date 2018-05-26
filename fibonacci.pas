7d3bce8696f5c55e0e0d2f2562aff9653762a0cb
axelbraunschweiger <axelbraunschweiger@gmail.com>
2018-05-26 17:24:49 -0500

    Fibonacci problem recursive solution

---
+++ fibonacci.pas
@@ -0,0 +1,24 @@
+
+program Fibonacci (input, output); 
+
+type
+tNatzahl = 1..maxint;
+
+function Fib (inZahl : tNatzahl): tNatZahl; 
+
+	begin 
+	if inZahl < 2 then
+	Fib := inZahl 
+	else
+	Fib := Fib(inZahl - 1) + Fib(inZahl -2)
+	end;
+	
+	
+	
+begin 
+
+writeln(Fib(8))
+
+end.
+	
+
