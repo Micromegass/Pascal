e1ba6728229bf29758b8123b32af753826664dea
axelbraunschweiger <axelbraunschweiger@gmail.com>
2018-07-25 18:08:15 -0500

    study

--- cons.pas
+++ cons.pas
@@ -1,17 +1,3 @@
 
 
 
-program (input, output); 
-const
-MAX = maxint; 
-
-type 
-tcharacter = 'a'..'z';
-tIndex = 1..MAX
-
-
-var 
-character: char; 
-Konsonant : tcharacter; 
-Vokal : tcharacter; 
-
