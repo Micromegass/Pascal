2acec694777a39ca139c5c2fc72a78d5ef5d6fbd
axelbraunschweiger <axelbraunschweiger@gmail.com>
2018-08-07 17:26:40 -0500

    twisting characters

---
+++ twist.pas
@@ -0,0 +1,26 @@
+
+
+
+program rek (input, output); 
+
+procedure twist;
+
+var
+zeichen : char; 
+
+begin 
+read (zeichen);
+if zeichen = '.' then 
+writeln
+else 
+begin 
+twist ;
+write(zeichen)
+end
+end;
+
+begin 
+
+twist()
+
+end.
