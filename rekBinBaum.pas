41ea047eb98638a14bf213eb5324e3c646d7b402
axelbraunschweiger <axelbraunschweiger@gmail.com>
2018-08-13 19:57:57 -0500

    study

--- rekBinBaum.pas
+++ rekBinBaum.pas
@@ -1,1 +1,38 @@
 
+
+
+program OBERE (input, output);
+
+type 
+tNatZahl = 0..maxint; 
+
+
+function ObereSchranke (inZahl:tNatZahl):tNatZahl;
+
+	var 
+	Zahl, 
+	Schranke : tNatZahl;
+	
+begin 
+
+ if inZahl = 0 then 
+ Schranke := 0
+ else 
+ begin 
+ Zahl := inZahl; 
+ Schranke := 1; 
+ 
+ repeat 
+ Zahl := Zahl div 10; 
+ Schranke := Schranke * 10 
+ until  Zahl = 0
+ end; {if}
+ 
+ ObereSchranke := Schranke
+ 
+end; 
+
+
+begin 
+write(ObereSchranke(100))
+end.
